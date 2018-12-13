d31dbb42b9cd024930e95c62b58b98f20bf34669
Artem Maevskiy <amaevskij@gmail.com>
2018-12-13 21:37:38 +0300

    stacked samples support

---
+++ run_tests_tf.py
@@ -0,0 +1,65 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from scipy.stats import ks_2samp
+
+import weighted_ks_tf as wks
+
+sess = tf.Session()
+
+x1 = tf.random_normal(shape=(tf.random_poisson(shape=[], lam=100, dtype='int32'), 1000))
+x2 = tf.random_normal(shape=(tf.random_poisson(shape=[], lam=100, dtype='int32'), 1000))
+np_x1, np_x2, wks_result = sess.run([x1, x2, wks.ks_2samp_w(x1, x2)])
+assert np.allclose(wks_result, [ks_2samp(np_x1[:,i], np_x2[:,i])[0] for i in range(1000)])
+
+x1 = np.insert(np.random.uniform(size=10), 0, [0.5])
+x2 = np.insert(np.random.uniform(size=5 ), 0, [0.5])
+
+print("my ks: {}".format(sess.run(wks.ks_2samp_w(x1, x2))[0]))
+print("scipy: {}".format(ks_2samp(x1, x2)[0]))
+
+x1 = x1[:,np.newaxis]
+x2 = x2[:,np.newaxis]
+
+cdf1 = wks.get_ecdf(x1)
+cdf2 = wks.get_ecdf(x2)
+
+def draw_cdf(x, y, **kwargs):
+    x, y = sess.run([x, y])
+    x = np.squeeze(x)
+    y = np.squeeze(y)
+    y = np.insert(y, np.arange(1, len(y)), y[:-1])
+    y = np.insert(y, 0, [0])
+    x = np.insert(x, np.arange(1, len(x)), x[1:])
+    x = np.insert(x, 0, x[0])
+    plt.plot(x, y, **kwargs)
+
+icdfs = wks._interleave_ecdfs(*cdf1, *cdf2)
+
+draw_cdf(*cdf1, label='cdf1', linewidth=1)
+draw_cdf(*cdf2, label='cdf2', linewidth=1)
+draw_cdf(icdfs[0], icdfs[1], label='cdf1 - interleaved', linewidth=4, alpha=0.3)
+draw_cdf(icdfs[0], icdfs[2], label='cdf2 - interleaved', linewidth=4, alpha=0.3)
+plt.legend()
+plt.show()
+
+#sample = np.random.normal(size=100000)
+#w = np.ones_like(sample)
+#w[(sample > 0) & (sample < 1.)] = 0.5
+#
+#print("ks: {}".format(wks.ks_2samp_w(sample, sample, None, w)))
+#
+#fig, ax1 = plt.subplots()
+#_, bins, _ = ax1.hist(sample, bins=100, alpha=0.5)
+#ax1.hist(sample, weights=w, bins=bins, histtype='step')
+#cdf1 = wks.get_ecdf(sample)
+#cdf2 = wks.get_ecdf(sample, w)
+#
+#icdfs = wks._interleave_ecdfs(*cdf1, *cdf2)
+#ax2 = ax1.twinx()
+#ax2.plot(icdfs[0], icdfs[1], label='normal')
+#ax2.plot(icdfs[0], icdfs[2], label='weighted')
+#ax2.legend()
+#
+#fig.tight_layout()
+#plt.show()
--- weighted_ks_tf.py
+++ weighted_ks_tf.py
@@ -4,6 +4,8 @@ from typing import Optional, Tuple
 
 import tensorflow as tf
 
+_T = tf.transpose
+
 def get_ecdf(
         sample: tf.Tensor,
         weights: Optional[tf.Tensor] = None
@@ -12,22 +14,25 @@ def get_ecdf(
     Get empirical CDF from a weighted 1D sample
     """
 
-    #assert len(sample.shape) == 1, "Only 1D CDF is implemented"
-
     if weights is None:
         weights = tf.ones_like(sample)
 
-    #assert sample.shape == weights.shape
-    
-    i = tf.contrib.framework.argsort(sample)
-    x, w = tf.gather(sample, i), tf.gather(weights, i)
-    
-    w_cumsum = tf.cumsum(w)
-    
-    #assert w_cumsum[-1] > 0
-    
-    w_cumsum /= w_cumsum[-1]
-    
+    with tf.control_dependencies([tf.assert_equal(tf.shape(sample), tf.shape(weights))]):
+        i = tf.contrib.framework.argsort(sample, axis=0)
+
+        x = _T(tf.batch_gather(
+                    _T(sample), _T(i)
+                ))
+        w = _T(tf.batch_gather(
+                    _T(weights), _T(i)
+                ))
+
+        w_cumsum = tf.cumsum(w, axis=0)
+
+        smallest_wsum = tf.reduce_min(w_cumsum[-1])
+        with tf.control_dependencies([tf.assert_greater(smallest_wsum, tf.zeros_like(smallest_wsum))]):
+            w_cumsum /= w_cumsum[-1]
+
     return x, w_cumsum
 
 
@@ -40,18 +45,20 @@ def _interleave_ecdfs(
     """
     Interleave two eCDFs by their argument
     """
-    #assert len(x1.shape) == len(x2.shape) == 1
-    #assert x1.shape == y1.shape
-    #assert x2.shape == y2.shape
 
-    x = tf.contrib.framework.sort(tf.concat([x1, x2], axis=0))
-    y1 = tf.concat([[0], y1], axis=0)
-    y2 = tf.concat([[0], y2], axis=0)
+    x = tf.contrib.framework.sort(tf.concat([x1, x2], axis=0), axis=0)
+    y1 = tf.concat([tf.zeros(shape=(1, tf.shape(y1)[1]), dtype=y1.dtype), y1], axis=0)
+    y2 = tf.concat([tf.zeros(shape=(1, tf.shape(y2)[1]), dtype=y2.dtype), y2], axis=0)
     return (
         x,
-        tf.gather(y1, tf.searchsorted(x1, x, side='right')),
-        tf.gather(y2, tf.searchsorted(x2, x, side='right'))
+        _T(tf.batch_gather(_T(y1), tf.searchsorted(_T(x1), _T(x), side='right'))),
+        _T(tf.batch_gather(_T(y2), tf.searchsorted(_T(x2), _T(x), side='right')))
     )
+
+def _make_rank2(x: tf.Tensor):
+    with tf.control_dependencies([tf.assert_rank_in(x, (1, 2))]):
+        result = tf.reshape(x, [tf.shape(x)[0], -1])
+    return result
 
 def ks_2samp_w(
             data1: tf.Tensor,
@@ -59,7 +66,21 @@ def ks_2samp_w(
             w1: Optional[tf.Tensor] = None,
             w2: Optional[tf.Tensor] = None
         ) -> float:
-    cdf1 = get_ecdf(data1, w1)
-    cdf2 = get_ecdf(data2, w2)
+    """
+    Calculate KS statistic on two weighted samples.
+    
+    The samples can be either 1D or 2D.
+    2D case is treated as if there were shape[1] samples of size shape[0]. Therefore,
+    the samples must have the same length along axis=1.
+    """
+    data1_ = _make_rank2(data1)
+    data2_ = _make_rank2(data2)
+    if w1 is not None:
+        w1 = _make_rank2(w1)
+    if w2 is not None:
+        w2 = _make_rank2(w2)
+
+    cdf1 = get_ecdf(data1_, w1)
+    cdf2 = get_ecdf(data2_, w2)
     _, cdf1_i, cdf2_i = _interleave_ecdfs(*cdf1, *cdf2)
-    return tf.reduce_max(tf.abs(cdf2_i - cdf1_i))
+    return tf.reduce_max(tf.abs(cdf2_i - cdf1_i), axis=0)
